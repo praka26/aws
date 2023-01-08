66241586c851eb00a4aea36dd41757eae6d4bab8
pravin kumar <pravin26@yahoo.com>
2023-01-08 13:50:19 -0700

    optuim 7

--- optum-demo/provider.tf
+++ optum-demo/provider.tf
@@ -3,10 +3,8 @@
 #############################################################################
 
 provider "aws" {
-  region = var.region
+  region = "${var.region}"
 
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  #access_key = "AKIAZZ5SAV3H2OXOPQH6"
-  #secret_key = "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"
-}
+ }
